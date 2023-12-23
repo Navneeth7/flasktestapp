2a0e8326851113cc6fcafeaa80db57aa53e95747
jokerlovesfrogs <154644161+jokerlovesfrogs@users.noreply.github.com>
2023-12-23 07:14:59 +0530

    Update test.py

--- test.py
+++ test.py
@@ -30,5 +30,4 @@ def hello():
     prediction=ml(data['n'],data['p'],data['k'],data['temp'],data['hum'],data['ph'])
     return prediction[0]
 if __name__ == '__main__':
-    app.debug = True
-    app.run()
+    app.run(host='0.0.0.0',debug=True)
