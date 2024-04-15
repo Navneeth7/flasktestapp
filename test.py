c36a66cb86a9f048f8007a2d34a48864f1ac6ed4
Karthik <84001885+KarthikNair45@users.noreply.github.com>
2024-04-15 22:15:49 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from flask import Flask
 import pickle
 import numpy as np
-
+import pandas as pd
 from flask import Flask, jsonify,request
 from flask_cors import CORS
 
@@ -23,11 +23,43 @@ def ml(n,p,k,temp,hum,ph):
     data = np.array([[n, p, k,temp,hum,ph]])
     prediction = dict1.predict(data)
     return prediction
+
+def ml2(temp,hum,mois,soilType,cropType,n,p,k):
+    with open('preprocessor.pkl', 'rb') as preprocessor_file, open('model.pkl', 'rb') as model_file:
+        preprocessor = pickle.load(preprocessor_file)
+        model = pickle.load(model_file)
+    new_sample = {
+    'Temparature': temp,
+    'Humidity': hum,
+    'Moisture': mois,
+    'Soil Type': soilType,
+    'Crop Type': cropType,
+    'Nitrogen': n,
+    'Potassium': p,
+    'Phosphorous': k
+    }
+    new_sample_df = pd.DataFrame([new_sample])
+
+    # Transform the new sample with the loaded preprocessor
+    new_sample_transformed = preprocessor.transform(new_sample_df)
+
+    # Use the loaded model to predict
+    prediction = model.predict(new_sample_transformed)
+
+    print(f"Predicted Fertilizer Name: {prediction[0]}")
+    return prediction[0]
 @app.route("/prediction",methods=["POST"])
 def hello():
     data=request.get_json()
     print(data)
     prediction=ml(data['n'],data['p'],data['k'],data['temp'],data['hum'],data['ph'])
     return prediction[0]
+@app.route("/fertprediction",methods=["POST"])
+def fert():
+    data=request.get_json()
+    print(data)
+    prediction=ml2(data['temp'],data['hum'],data['mois'],data['soilType'],data['cropType'],data['n'],data['p'],data['k'])
+    print(prediction)
+    return prediction
 if __name__ == '__main__':
     app.run(host='0.0.0.0',debug=True)
